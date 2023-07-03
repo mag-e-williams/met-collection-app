503b24aa907b798bcf79a540f06a8994380bf45f
mag-e-williams <margretwilliams42@gmail.com>
2023-07-03 15:40:56 -0400

    tmp

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { Container } from '@mui/material';
+import { Box, Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, Stack, Typography } from '@mui/material';
 
 import { fetchMetData } from '@/api/fetchMetData';
 import { MetObject } from '@/types/MetObject';
@@ -29,62 +29,104 @@ const COLS = [
   },
 ]
 
-
 export function Homepage() {
   const router = useRouter();
   const [museumObjects, setMuseumObjects] = useState<MetObject[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [searchTerm, setSearchTerm] = useState<string>();
+  const [totalPages, setTotalPages] = useState<number>();
+
+  const inputRef = React.useRef<HTMLInputElement>(null); // Define inputRef here
 
   useEffect(() => {
     const fetchObjects = async () => {
-      const response = await fetchMetData();
+      const response = await fetchMetData(searchTerm);
+      console.log(response)
       setMuseumObjects(response);
     }
 
     fetchObjects()
-  }, []);
+  }, [page, searchTerm]);
 
+  const updateURL = (selectedPage: number, term: string) => {
+    const queryParams = new URLSearchParams({
+      page: selectedPage.toString(),
+      search: term,
+    });
 
-  const handleSearch = async (event: React.ChangeEvent<HTMLInputElement>) => {
-    const term = event.target.value;
-    // setPage(1);
-    // setSearchTerm(term);
-    // updateURL(1, term);
+    router.push(`/?${queryParams.toString()}`);
   };
 
+  const handleSearch = async (event: React.FocusEventHandler<HTMLInputElement>) => {
+    const term = event.target.value;
+    console.log(term)
+    setPage(page);
+    setSearchTerm(term);
+    updateURL(page, term);
+  };
 
   return (
     <>
-      <Header searchTerm={'vaccums'} onSearchTerm={handleSearch} />
+      <Header searchTerm={searchTerm} onSearchTerm={handleSearch} />
       
       <Container sx={{ marginTop: 16}}>
-        <table>
-          <tr>
-            {COLS.map(col => {
-              return (
-                <th key={col.id}>
-                  <h5>{col.name}
-                  </h5>
-                </th>
-              )
-            })}
-          </tr>
-        {museumObjects.map((e) => {
-          console.log(e)
-          return (
-          <tr key={e.objectID}>
-            {COLS.map(col => {
-              return (
-                <td key={col.id}>
-                  <p>{e[col.id as keyof MetObject]}</p>
-                </td>
-              )
-            })}
+        <Stack spacing={2}>
 
-          </tr>
-        )})}
-        </table>
-    </Container>
-    </>
+        {museumObjects.map(obj => (
+          <Card sx={{ display: 'flex' }} key={obj.objectID}>
+          
+          <CardMedia
+            component="img"
+            sx={{ width: 151}}
+            image={obj.primaryImage}
+            alt="No Image Found"
+          />
+
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+
+            <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography
+                  variant="h5"
+                  noWrap
+                  component="a"
+                  href={obj.objectURL}
+                  target="_blank"
+                  sx={{
+                    mr: 2,
+                    display: { md: 'flex' },
+                    textDecoration: 'none',
+                    flexGrow: 1,
+                  }}
+                >
+                {obj.title}
+              </Typography>
+
+              <Typography variant="body1" component="div">
+                {obj.department} 
+              </Typography>
+
+              <Typography variant="body1" component="div">
+                {obj.objectDate} 
+              </Typography>
+
+              <Typography variant="body1" component="div">
+                {obj.creditLine}
+              </Typography>
+                
+            <Typography variant="body1" component="div">
+              {obj.isHighlight ? 'On Display' : 'Not On Display'}
+            </Typography>
+            </CardContent>
+
+          </Box>
+
+        </Card>
+
+        ))}
+        </Stack>
+
+      </Container>
     
+    </>
   );
 }
