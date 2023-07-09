1e237d27a58b928edf77af2184879c2e39c5dee6
mag-e-williams <margretwilliams42@gmail.com>
2023-07-09 18:45:37 -0400

    routing and api logic overhaul

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,16 +1,9 @@
-// import type { EndpointKey } from 'api/endpoints';
-// import { FetchedFallbackData } from 'api/fetchFallbackData';
-// import { Footer } from 'components/nav/Footer';
-// import { Header } from 'components/nav/Header';
-// import { Meta } from 'components/utilComponents/Meta';
-// import { ScrollIndicatorContext } from 'components/nav/ScrollIndicatorContext';
-// import { useShowScrollIndicator } from 'hooks/useShowScrollIndicator';
-import Header from '../Header';
-
 import { SWRConfig } from 'swr';
 import { Container } from '@mui/material';
 import { FetchedFallbackData } from '@/api/fetchFallbackData';
 import { EndpointKey } from '@/api/endpoints';
+import Header from '@/components/Header';
+import { Meta } from '../utilComponents/Meta';
 
 type PageLayoutProps<Keys extends EndpointKey> = {
   children: React.ReactNode;
@@ -20,6 +13,7 @@ type PageLayoutProps<Keys extends EndpointKey> = {
 export function PageLayout<Key extends EndpointKey>({ children, fallback }: PageLayoutProps<Key>) {
   return (
     <SWRConfig value={{ fallback }}>
+      <Meta />
       <Header  />
       <Container>
         <main>{children}</main>
